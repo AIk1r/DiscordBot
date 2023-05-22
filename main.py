87e2138383c985ce3c8a3ae109e5ec8629076094
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-05-23 00:44:33 +0700

    Add command mute users

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 WELCOME_CHANNEL = os.getenv('DISCORD_WELCOME_CHANNEL')
 GENERAL_ROLE = os.getenv('DISCORD_GENERAL_ROLE')
+MUTE_ROLE = os.getenv('DISCORD_MUTE_ROLE')
 
 
 @bot.event
@@ -71,6 +72,20 @@ async def avatar(ctx, member: discord.Member  = None):
     embed = discord.Embed(color = 0x8B0000, title = f"Member's avatar - {member.name}", description = f"[Click to download the avatar]({member.avatar})")
     embed.set_image(url = member.avatar)
     await ctx.send(embed = embed)
+    
+    
+@bot.command()
+@commands.has_permissions(kick_members = True)
+async def mute(ctx, member : discord.Member):
+    muteRole = ctx.guild.get_role(int(MUTE_ROLE))
+    for i in member.roles:
+        try:
+            await member.remove_roles(i)
+        except:
+            print(f"Can't remove the role {i}")
+    await member.add_roles(muteRole)
+    await ctx.channel.purge(limit=1)
+    await ctx.send(str(member) + ' has been muted!')
 
 
 
